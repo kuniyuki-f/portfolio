feca07dbac3c952aa6e316eaf667c34c30cae471
c501306014 <c501306014@gmail.com>
2023-11-24 20:16:43 +0900

    :wrench: Astroでmdxを使えるようにした

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-
+import mdx from "@astrojs/mdx";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// https://astro.build/config
 export default defineConfig({
   vite: {
     resolve: {
       alias: {
         '@/': `${path.resolve(__dirname, 'src')}/`
       }
-    },
-  }
+    }
+  },
+  integrations: [mdx()]
 });
