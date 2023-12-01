2d525bae1a1b2e2427464654a5b3f2f4e53ab30e
c501306014 <c501306014@gmail.com>
2023-12-01 20:06:23 +0900

    :wrench: #19 Astro に rehype-pretty-code の設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,33 @@ import { defineConfig } from 'astro/config';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import mdx from "@astrojs/mdx";
+import rehypePrettyCode from 'rehype-pretty-code';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const prettyCodeOptions = {
+  theme: "material-theme-palenight",
+  onVisitLine(node) {
+    const nodeClass = node.properties.className;
+    if (node.children.length === 0) {
+      node.children = [{ type: 'text', value: ' ' }];
+    }
+    if (nodeClass && nodeClass.length > 0) {
+      node.properties.className.push('line');
+    } else {
+      node.properties.className = ['line'];
+    }
+  },
+  onVisitHighlightedLine(node) {
+    const nodeClass = node.properties.className;
+    if (nodeClass && nodeClass.length > 0) {
+      node.properties.className.push('highlighted');
+    } else {
+      node.properties.className = ['highlighted'];
+    }
+  },
+};
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -25,5 +50,9 @@ export default defineConfig({
       }
     }
   },
-  integrations: [mdx()]
+  integrations: [mdx(),],
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+  }
 });
